ade179b700985cac7f75c6d5ffcca87d0613e46d
MrC2020 <careyma@live.com>
2016-01-08 13:34:29 -0500

    Yeoman Example
    Working with Yeoman, bower and grunt

---
+++ test-app/app/scripts/core.js
@@ -0,0 +1,10 @@
+﻿/* global define */
+define(function () {
+    'use strict';
+
+    var self = {};
+
+    console.log('Modiule Ready for Action');
+
+    return self;
+});
---
+++ test-app/app/scripts/main.js
@@ -0,0 +1,6 @@
+require(['core'], function (core) {
+    'use strict';
+
+    console.log('Core module loaded ', core);
+});
+
